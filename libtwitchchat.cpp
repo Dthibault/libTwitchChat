97f6a65525be8988eeb6519ae678142bf997457e
Thibault Dupuis <thib.dupuis@gmail.com>
2016-05-04 17:41:21 +0200

    Add the Doxygen documentation for the project

--- libtwitchchat.cpp
+++ libtwitchchat.cpp
@@ -1,5 +1,17 @@
+/**
+  * @file libtwitchchat.cpp
+  * @brief C++/Qt Library for Twitch Chat
+  * @author Thibault "Palamecia" Dupuis
+  * @version 0.0.1
+  * @date 05/04/2016
+  *
+  * C++/Qt Library for Twitch Chat. This library is for bot use.
+  *
+  */
+
 #include "libtwitchchat.h"
 
+// Constructor for the libTwitchChat class
 libTwitchChat::libTwitchChat(QObject *parent) : QObject(parent)
 {
     this->debugState = false;
@@ -8,16 +20,19 @@ libTwitchChat::libTwitchChat(QObject *parent) : QObject(parent)
     this->botToken = QString();
 }
 
+// Destructor for the libTwitchChat class
 libTwitchChat::~libTwitchChat()
 {
 
 }
 
+// Enable/Disable the debug mode
 void libTwitchChat::setDebug(bool state)
 {
     this->debugState = state;
 }
 
+// Set a new name for the bot
 void libTwitchChat::setBotName(QString newName)
 {
     this->botName = newName;
@@ -26,6 +41,20 @@ void libTwitchChat::setBotName(QString newName)
         qDebug() << "New name for the bot: " << newName;
 }
 
+
+// Get the name of the bot
+QString libTwitchChat::getBotName()
+{
+    return this->botName;
+}
+
+// Get the token of the bot
+QString libTwitchChat::getBotToken()
+{
+    return this->botToken;
+}
+
+// Set a new token for the bot
 void libTwitchChat::setBotToken(QString newToken)
 {
     this->botToken = newToken;
--- libtwitchchat.h
+++ libtwitchchat.h
@@ -1,3 +1,14 @@
+/**
+  * @file libtwitchchat.h
+  * @brief C++/Qt Library for Twitch Chat
+  * @author Thibault "Palamecia" Dupuis
+  * @version 0.0.1
+  * @date 05/04/2016
+  *
+  * C++/Qt Library for Twitch Chat. This library is for bot use.
+  *
+  */
+
 #ifndef LIBTWITCHCHAT_H
 #define LIBTWITCHCHAT_H
 
@@ -7,24 +18,69 @@
 
 #define LIBTWITCHCHAT_VERSION "0.0.1"
 
+/**
+ * @class libTwitchChat
+ * @brief The libTwitchChat class
+ *
+ * This class is used for the whole library
+ */
 class libTwitchChat : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructor
+     *
+     * Constructor for the libTwitchChat class
+     *
+     * @param parent
+     */
     explicit libTwitchChat(QObject *parent = 0);
+
+    /**
+     * @brief Destructor
+     *
+     * Destructor for the libTwitchChat class
+     *
+     */
     virtual ~libTwitchChat();
 
+    /**
+     * @brief Enable/Disable the debug mode
+     * @param state True/False for enable/disable the debug mode
+     */
     void setDebug(bool state);
 
+    /**
+     * @brief Set a new name for the bot
+     * @param newName Name of the bot
+     */
     void setBotName(QString newName);
+
+    /**
+     * @brief Get the name of the bot
+     * @return The bot name
+     */
+    QString getBotName();
+
+    /**
+     * @brief Set a new token for the bot
+     * @param newToken Token of the bot
+     */
     void setBotToken(QString newToken);
+
+    /**
+     * @brief Get the token of the bot
+     * @return The bot token
+     */
+    QString getBotToken();
 
 private:
 
-    bool debugState;
+    bool debugState; /*!< Status of the debug mode */
 
-    QString botName;
-    QString botToken;
+    QString botName; /*!< Bot name */
+    QString botToken; /*!< Bot token */
 signals:
 
 public slots:
